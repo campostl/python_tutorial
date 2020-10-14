3695690e9f8a8f4868a7d48d6a12a7ca1e7ff9ef
Teresa Campos <campos@vpn68-10.vpnclient.ucar.edu>
2020-10-14 14:02:38 -0600

    Adding commented out lines also used in 1st tutorial

--- data/mysci.py
+++ data/mysci.py
@@ -2,4 +2,12 @@
 filename = "wxobs20170821.txt"
 with open(filename,'r') as datafile:
    data = datafile.read()
-
+# this file read method uses a context manager
+# other way: 
+#
+# datafile = open(filename,'r')
+# data = datafile.read()
+# datafile.close()
+# # Debug
+# print(data)
+# print(type(data))
