9396b7261fdc2b1b1e26025e1f99ce69fc57cce3
Teresa Campos <campos@vpn68-12.vpnclient.ucar.edu>
2020-11-11 13:14:40 -0700

    Reading windspeed as well

--- data/mysci.py
+++ data/mysci.py
@@ -1,8 +1,8 @@
 # Column names an dcolumn indices to read
-columns = {'date':0, 'time':1, 'tempout':2}
+columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7}
 
 #Data types for each column (only if non-string)
-types = {'tempout':float}
+types = {'tempout':float, 'windspeed': float}
 
 # Initialize my data as a data dictionary; 
 # curly brackets => dictionary keys are strings, colon maps
@@ -17,6 +17,8 @@ with open(filename,'r') as datafile:
    # read the first three lines (header) using a for loop
    # underscore is for variable used only as index
    for _ in range(3):
+#       headerline=datafile.readline()
+#       print(headerline)
        print(_)
        datafile.readline()
 
@@ -29,27 +31,7 @@ with open(filename,'r') as datafile:
            value = t(split_line[i])
            data[column].append(value)
 
-# line.split('\t') for tab delimiter, etc. default is whitespace
-#       data.append(datum) 
-
-# old DEBUG
-#for datum in data[:10:2]:
-#    print(datum)
-# print(data[-1])
-# [-1] implies last data pt
-#for datum in data:
-#      print(datum)
-
-#with open(filename,'r') as datafile:
-#   data = datafile.read()
-# this file read method uses a context manager
-# other way: 
-#
-# datafile = open(filename,'r')
-# data = datafile.read()
-# datafile.close()
-# # Debug
-# print(data)
-# print(type(data))
+# DEBUG
+print(data['tempout'])
 
 
