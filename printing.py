72f46836b272c5efecaf68029b2215b6ef31c2db
Teresa Campos <campos@ucar.edu>
2021-01-27 15:55:52 -0700

    Creating printing and computing modules

--- printing.py
+++ printing.py
@@ -1,4 +1,4 @@
-def print_comparision(name, dates, times, original_data, computed_data):
+def print_comparison(name, dates, times, original_data, computed_data):
     """
     Print a comparison of two time series (original and computed)
 
@@ -9,12 +9,10 @@ def print_comparision(name, dates, times, original_data, computed_data):
        original_data: List of original data (floats)
        computed_data: List of computed data (floats)
     """
-
-# Output comparison of data
-print(f'                ORIGINAL   COMPUTED')
-print(f' DATE   TIME   {name.upper():>9} {name.upper():>9} DIFFERENCE')
-print(f'------- ------ --------- --------- ----------')
-zip_data = zip(data['date'], data['time'], data['heatindex'], heatindex)
-for date, time, orig, comp in zip(dates, times, original_data, computed_data):
-    print(f'{date} {time:>6} {orig:9.6f} {comp:9.6f} {orig-comp:10.6f}')
+    # Output comparison of data
+    print(f'                ORIGINAL   COMPUTED')
+    print(f' DATE   TIME   {name.upper():>9} {name.upper():>9} DIFFERENCE')
+    print(f'------- ------ --------- --------- ----------')
+    for date, time, orig, comp in zip(dates, times, original_data, computed_data):
+       print(f'{date} {time:>6} {orig:9.6f} {comp:9.6f} {orig-comp:10.6f}')
 
--- windchillcomp.py
+++ windchillcomp.py
@@ -1,4 +1,6 @@
 from readdata import read_data
+from printing import print_comparison
+from computation import compute_windchill
 
 # Column names an dcolumn indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7, 'windchill':12}
@@ -9,18 +11,7 @@ types = {'tempout':float, 'windspeed': float, 'windchill': float}
 # Read data from file
 data = read_data(columns, types=types)
 
-# Compute the wind chill temperature
-def compute_windchill(t, v):
-    a = 35.74
-    b = 0.6215
-    c = 35.75
-    d = 0.4275
-
-    v16 = v ** 0.16
-    wci = a + (b * t) - (c * v16) + (d * t * v16)
-    return wci
-
-# Running the function to compute windchill index
+# Compute the wind chill factor
 windchill = []
 for temp, windspeed in zip(data['tempout'], data['windspeed']):
     windchill.append(compute_windchill(temp, windspeed))
